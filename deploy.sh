14a2a09a7519e13fcce6785986053e0132a0677d
tomasz.bar3@gmail.com <Niuteczka1!>
2019-07-13 08:43:15 +0200

    Add Server

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 
 if [ $1 == "-c" ]; then
     ssh pi@192.168.1.1 git -C /home/pi/Projects/nit-home/nit-home-web pull
-else    
-    echo "Strings are not equal"
 fi
 
 #upload build
 scp -rp build/ pi@192.168.1.1:/home/pi/Projects/nit-home/nit-home-web
-#run server
+
+#run devices wrokers
+ssh root@192.168.1.1 python3 /home/pi/Projects/nit-home/server/led-ws-worker.py &
+
+#run web server
 ssh root@192.168.1.1 cd /home/pi/Projects/nit-home/nit-home-web | npm run prod-start
