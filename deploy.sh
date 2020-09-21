9b258128f11dd4df3a2e996341265909e998f383
Tomek B <tomasz.bar3@gmail.com>
2020-09-21 17:19:30 +0200

    nowe wydanie

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 if [ $1 == "-c" ]; then
     git commit -a -m "$0"
     git push
-    ssh pi@192.168.1.1 git -C /home/pi/Projects/nit-home/nit-home-web pull
+    ssh pi@192.168.55.1 git -C /home/pi/Projects/nit-home/nit-home-web pull
 fi
 
 #kill old process
@@ -10,10 +10,10 @@ fi
 # ssh root@192.168.1.1 killall -9 python3
 
 #upload build
-scp -rp build/ pi@192.168.1.1:/home/pi/Projects/nit-home/nit-home-web
+scp -rp build/ pi@192.168.55.102:/home/pi/Projects/nit-home/nit-home-web
 
 #run devices wrokers
-ssh root@192.168.1.1 python3 /home/pi/Projects/nit-home/server/led-ws-worker.py > /home/pi/Projects/nit-home/server/log.txt &
+ssh root@192.168.55.102 python3 /home/pi/Projects/nit-home/server/led-ws-worker.py > /home/pi/Projects/nit-home/server/log.txt &
 
 #run web server
-ssh root@192.168.1.1 cd /home/pi/Projects/nit-home/nit-home-web | npm run prod-start
+ssh root@192.168.55.102 cd /home/pi/Projects/nit-home/nit-home-web | npm run prod-start
--- src/App.tsx
+++ src/App.tsx
@@ -2,64 +2,57 @@ import React, { ChangeEvent, useRef, useState, useEffect } from 'react';
 import './App.css';
 import styled from 'styled-components';
 import Slider from '@material-ui/core/Slider';
+import Button from '@material-ui/core/Button'
+import axios from 'axios'
 
-console.log("RERENDER")
+const serverUrl = "http://192.168.55.102:5000/api/strip/color";
+
+type Rgb = {
+  r: number,
+  g: number,
+  b: number
+}
 
 function App() {
 
-  const LED_COUNT = 150;
-  const [red, setRed] = useState<number>(0)
-  const [green, setGreen] = useState(0)
-  const [blue, setBlue] = useState(0)
-  const [wsClient, setWsClient] = useState<WebSocket>()
+  const [currentColor, setCurrentColor] = useState<Rgb>({
+      r: 0,
+      g: 0,
+      b: 0
+  })
 
-  useEffect(() => {
-    console.log("CRATE WEB SCOKET")
-    setWsClient(new WebSocket("ws://192.168.1.1:8765"))
-  }, [])
-
-  const setLed = (id: number, r: number, g: number, b:number) => {
-    if (wsClient!.readyState === wsClient!.OPEN) {
-      console.log("SENDING...")
-      wsClient!.send(`${id} ${r} ${g} ${b}`)
-    }
+  const setRed = (red: number) => {
+    const newColor = {...currentColor, r: red}
+    setCurrentColor(newColor)
+    displayLedColor(newColor)
   }
 
-  const onRedChange = (event: ChangeEvent<{}>, value: number | number[]) => {
-    console.log(wsClient)
-    setRed(value as number)
-    
-    for(var i = 0 ;i<LED_COUNT; i++) {
-      setLed(i, red, green, blue);
-   }
+  const setGreen = (green: number) => {
+    const newColor = {...currentColor, g: green}
+    setCurrentColor(newColor)
+    displayLedColor(newColor)
   }
 
-
-  const onGreenChange = (event: ChangeEvent<{}>, value: number | number[]) => {
-    setGreen(value as number)
-    for(var i = 0 ;i<LED_COUNT; i++) {
-      setLed(i, red, green, blue);
-   }
+  const setBlue = (blue: number) => {
+    const newColor = {...currentColor, b: blue}
+    setCurrentColor(newColor)
+    displayLedColor(newColor)
   }
 
-  const onBlueChange = (event: ChangeEvent<{}>, value: number | number[]) => {
-    setBlue(value as number)
-    for(var i = 0 ;i<LED_COUNT; i++) {
-      setLed(i, red, green, blue);
-   }
+  const displayLedColor = (color: Rgb) => {
+      axios.post(serverUrl, color)
   }
 
   return (
     <div className="App">
       <SliderBox>
-        Green: <Slider min={0} max={255} value={red} onChange={onRedChange}></Slider>
+        <Slider min={0} max={1} step={0.05} value={currentColor.r} onChange={(_, value: number|number[]) => setRed(value as number)}/>
+        <Slider min={0} max={1} step={0.05} value={currentColor.g} onChange={(_, value: number|number[]) => setGreen(value as number)}/>
+        <Slider min={0} max={1} step={0.05} value={currentColor.b} onChange={(_, value: number|number[]) => setBlue(value as number)}/>
       </SliderBox>
-      <SliderBox>
-        Red: <Slider min={0} max={255} value={green} onChange={onGreenChange}></Slider>
-      </SliderBox>
-      <SliderBox>
-        Blue: <Slider min={0} max={255} value={blue} onChange={onBlueChange}></Slider>
-      </SliderBox>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <Button color="primary" variant="contained" onClick={()=>displayLedColor({r: 0, g: 0, b:0})}>Wyłącz</Button>
+      </div>
     </div>
   );
 }
@@ -69,9 +62,7 @@ const SliderBox = styled.div`
   margin-top: 20px;
   margin-left: 30px;
   margin-right: 30px;
-  width: 80%;
   height: 100px;
-  background-color: "#eeeeee"
 `
 
 export default App;
